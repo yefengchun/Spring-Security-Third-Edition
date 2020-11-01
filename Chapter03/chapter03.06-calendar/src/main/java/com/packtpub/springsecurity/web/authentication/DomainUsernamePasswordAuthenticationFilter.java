e311c6055dbf7aa45ccf8dec24d13ca0d4ee009b
Ye FengChun <yefengchun@github.com>
2020-11-01 17:18:43 +0800

    Add Filter class.

--- Chapter03/chapter03.06-calendar/src/main/java/com/packtpub/springsecurity/web/authentication/DomainUsernamePasswordAuthenticationFilter.java
+++ Chapter03/chapter03.06-calendar/src/main/java/com/packtpub/springsecurity/web/authentication/DomainUsernamePasswordAuthenticationFilter.java
@@ -1,4 +1,60 @@
 package com.packtpub.springsecurity.web.authentication;
 
-public class DomainUsernamePasswordAuthenticationFilter {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.packtpub.springsecurity.authentication.DomainUsernamePasswordAuthenticationToken;
+
+/**
+ * An extension to the existing {@link UsernamePasswordAuthenticationFilter} that obtains a domain parameter and then
+ * creates a {@link DomainUsernamePasswordAuthenticationToken}.
+ *
+ * @author Rob Winch
+ */
+//public final class DomainUsernamePasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+//
+//    public DomainUsernamePasswordAuthenticationFilter(AuthenticationManager authenticationManagerBean) {
+//    }
+//
+//    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+//            throws AuthenticationException {
+//        if (!request.getMethod().equals("POST")) {
+//            throw new AuthenticationServiceException("Authentication method not supported: " + request.getMethod());
+//        }
+//
+//        String username = obtainUsername(request);
+//        String password = obtainPassword(request);
+//        String domain = request.getParameter("domain");
+//
+//        DomainUsernamePasswordAuthenticationToken authRequest = new DomainUsernamePasswordAuthenticationToken(username,
+//                password, domain);
+//
+//        setDetails(request, authRequest);
+//        return this.getAuthenticationManager().authenticate(authRequest);
+//    }
+//}
+public final class DomainUsernamePasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException
+    {
+        if (!request.getMethod().equals("POST")) {
+            throw new AuthenticationServiceException ("Authentication method not supported: " + request.getMethod());
+        }
+        String username = obtainUsername(request);
+        String password = obtainPassword(request);
+        String domain = request.getParameter("domain");
+// authRequest.isAuthenticated() = false since no
+//authorities are specified
+        DomainUsernamePasswordAuthenticationToken authRequest
+                = new DomainUsernamePasswordAuthenticationToken(username, password, domain);
+        setDetails(request, authRequest);
+        return this.getAuthenticationManager()
+                .authenticate(authRequest);
+    }
 }
